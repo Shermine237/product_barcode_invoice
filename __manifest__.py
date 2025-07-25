5bd0016f4f6f26e8340b405558d3db5f03d43814
Charlie Rostant YOSSA <charlieyossa@gmail.com>
2025-07-25 14:41:26 +0200

    First commit

---
+++ __manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name": "Product Barcode on Invoice Lines",
+    "version": "1.0",
+    "author": "ABCD",
+    "depends": ["account", "product"],
+    "data": [
+        "views/account_move_form.xml",
+        "views/report_invoice_barcode.xml",
+    ],
+    "installable": True,
+    "application": False,
+}
---
+++ models/account_move_line.py
@@ -0,0 +1,16 @@
+from odoo import models, fields, api
+
+class AccountMoveLine(models.Model):
+    _inherit = 'account.move.line'
+
+    product_barcode = fields.Char(
+        string="Code-barres",
+        compute="_compute_product_barcode",
+        store=False,
+        readonly=True
+    )
+
+    @api.depends('product_id')
+    def _compute_product_barcode(self):
+        for line in self:
+            line.product_barcode = line.product_id.barcode or ''
