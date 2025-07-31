a7688ec9ceba45c4dcd8d27f39c5b50e8b01a570
Charlie Rostant YOSSA <charlieyossa@gmail.com>
2025-07-31 12:06:47 +0200

    Add barrecode in sale and purchase

--- __manifest__.py
+++ __manifest__.py
@@ -1,11 +1,15 @@
 {
-    "name": "Product Barcode on Invoice Lines",
-    "version": "1.0",
+    "name": "Product Barcode on Documents",
+    "version": "2.0",
     "author": "ABCD",
-    "depends": ["account", "product"],
+    "depends": ["account", "product", "sale", "purchase"],
     "data": [
         "views/account_move_form.xml",
         "views/report_invoice_barcode.xml",
+        "views/sale_order_form.xml",
+        "views/purchase_order_form.xml",
+        "views/report_saleorder_barcode.xml",
+        "views/report_purchaseorder_barcode.xml",
     ],
     "installable": True,
     "application": False,
---
+++ models/sale_purchase_barcode.py
@@ -0,0 +1,23 @@
+from odoo import models, fields, api
+
+
+class SaleOrderLine(models.Model):
+    _inherit = 'sale.order.line'
+
+    product_barcode = fields.Char(string="Code-barres", compute="_compute_product_barcode", store=False, readonly=True)
+
+    @api.depends('product_id')
+    def _compute_product_barcode(self):
+        for line in self:
+            line.product_barcode = line.product_id.barcode or ''
+
+
+class PurchaseOrderLine(models.Model):
+    _inherit = 'purchase.order.line'
+
+    product_barcode = fields.Char(string="Code-barres", compute="_compute_product_barcode", store=False, readonly=True)
+
+    @api.depends('product_id')
+    def _compute_product_barcode(self):
+        for line in self:
+            line.product_barcode = line.product_id.barcode or ''
