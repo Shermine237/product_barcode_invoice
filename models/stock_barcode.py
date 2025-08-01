6a76deedf21583aa58313bb36ee7161e5279f088
Charlie Rostant YOSSA <charlieyossa@gmail.com>
2025-08-01 11:19:48 +0200

    [FIX] PDF stock_picking

--- models/stock_barcode.py
+++ models/stock_barcode.py
@@ -14,20 +14,6 @@ class StockMoveLine(models.Model):
     )
 
 
-class StockPicking(models.Model):
-    _inherit = 'stock.picking'
-
-    def _get_delivery_orders_and_lines_data(self):
-        """ Override the main report data generation to inject the barcode. """
-        data = super()._get_delivery_orders_and_lines_data()
-        for order_data in data.get('orders', []):
-            if 'aggregated_lines' in order_data:
-                for product_id, line_vals in order_data['aggregated_lines'].items():
-                    product = self.env['product.product'].browse(product_id)
-                    line_vals['barcode'] = product.barcode or ''
-        return data
-
-
 # Also add barcode on stock.move (parent move), used in main operations list
 
 class StockMove(models.Model):
